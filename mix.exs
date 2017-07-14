a28e921de9a1b6b02056b8f40d3a05b948ed1a84
Krzysztof Wende <chris@neontree.pl>
2017-07-14 20:11:16 +0200

    Release 0.4.8

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Elchemy.Mixfile do
     [app: :elchemy,
      name: "Elchemy Compiler",
      description: "Mix compiler wrapper around Elchemy project",
-     version: "0.4.7",
+     version: "0.4.8",
      elixir: "~> 1.4",
      description: "",
      package: package(),
@@ -34,11 +34,11 @@ defmodule Elchemy.Mixfile do
 
   # Dependencies can be Hex packages:
   #
-  #   {:my_dep, "~> 0.4.7"}
+  #   {:my_dep, "~> 0.4.8"}
   #
   # Or git/path repositories:
   #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.4.7"}
+  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.4.8"}
   #
   # Type "mix help deps" for more examples and options
   defp deps do
