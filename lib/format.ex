c595e801a278c3720a461ae56e22e2b2c32d5dab
Kacper Latuszewski <kacpx123@gmail.com>
2018-05-02 17:10:35 +0200

    Format. Test 2/5

--- lib/format.ex
+++ lib/format.ex
@@ -2,8 +2,25 @@ defmodule Elchemy.Format do
 
   @doc "Some doc"
   @spec inspect(term()) :: String.t()
-  def inspect(term) do
-    # Tutaj kod zamiast tego
-    Kernel.inspect(term)
+  def inspect(term) when is_tuple(term) do
+      term |> Tuple.to_list() |> Enum.join(" ") |> String.capitalize
   end
+
+  def inspect(term) when is_map(term) do
+    inner = term |> Enum.map(fn {key, value} ->
+      (key |> Kernel.to_string) <> " = " <> (value |> Kernel.to_string)
+    end)
+    "{ " <> Enum.join(inner, ", ") <> " }"
+  end
+
+  def inspect(term) when is_tuple(term) do
+    [head | rest] = term |> Tuple.to_list
+    if is_atom(head) do
+      false
+    else
+      [head | rest] = [nil | (Tuple.to_list(rest) |> List.to_string(rest))]
+      "(" <> Enum.join(rest, ", ") <> ")"
+    end
+  end
+
 end
