80d79f1000f0211ac7695ea9b39672e880fc655e
Kacper Latuszewski <kacpx123@gmail.com>
2018-05-03 17:50:53 +0200

    Tests 7/8

--- lib/format.ex
+++ lib/format.ex
@@ -6,16 +6,28 @@ defmodule Elchemy.Format do
   @spec inspect(term()) :: String.t()
 
   def inspect(term) when is_map(term) do
-    inner = term |> Enum.map(fn {key, value} ->
-      (key |> inspect()) <> " = " <> (value |> inspect())
-    end)
-    "{ " <> Enum.join(inner, ", ") <> " }"
+    case Map.to_list(term) do
+      [] -> "Dict.fromList []"
+    [{key, _value} | _] when is_atom(key) ->
+      inner = term |> Enum.map(fn {key, value} ->
+        (key |> inspect()) <> " = " <> (value |> inspect())
+      end)
+      "{ " <> Enum.join(inner, ", ") <> " }"
+    [{_key, _value} | _] ->
+      inner = term |> Enum.map(fn {key, value} ->
+        (key |> inspect()) <> " = " <> (value |> inspect())
+      end)
+      "Dict.fromList [{ " <> Enum.join(inner, ", ") <> " }]"
+    end
+
   end
 
   def inspect(term) when is_tuple(term) do
       [head | rest] = term |> Tuple.to_list
       if is_atom(head) do
-          (head |> Atom.to_string() |> String.capitalize()) <> " " <> (rest |> Tuple.to_list() |> Enum.map(&inspect/1) |> Enum.join(" "))
+          left = (head |> Atom.to_string() |> String.capitalize())
+          right = (rest |> Enum.map(&inspect/1) |> Enum.join(" "))
+          left <> " " <> right
       else
         inner = term |> Tuple.to_list() |> Enum.map(&inspect/1)
         "(" <> Enum.join(inner, ", ") <> ")"
