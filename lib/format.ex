44c07de9b82bf99050d134e84a5e143fd79c7912
Krzysztof Wende <krzysztof.wende@gmail.com>
2018-05-04 18:03:31 +0200

    Parens

--- lib/format.ex
+++ lib/format.ex
@@ -1,16 +1,16 @@
 defmodule Elchemy.Format do
 
-  import Kernel, except: [inspect: 1]
+  import Kernel, except: [inspect: 1, inspect: 2]
 
   @doc "Some doc"
   @spec inspect(term()) :: String.t()
-
-  def inspect(term) when is_map(term) do
+  def inspect(term, in_type \\ false)
+  def inspect(term, _) when is_map(term) do
     case Map.to_list(term) do
       [] -> "Dict.fromList []"
     [{key, _value} | _] when is_atom(key) ->
       inner = term |> Enum.map(fn {key, value} ->
-        (key |> inspect()) <> " = " <> (value |> inspect())
+        (key |> to_string()) <> " = " <> (value |> inspect())
       end)
       "{ " <> Enum.join(inner, ", ") <> " }"
     [{_key, _value} | _] ->
@@ -24,16 +24,18 @@ defmodule Elchemy.Format do
   def inspect(term, in_type) when is_tuple(term) do
       [head | rest] = term |> Tuple.to_list
       if is_atom(head) do
-          left = (head |> Atom.to_string() |> String.capitalize())
-          right = (rest |> Enum.map(&inspect/1) |> Enum.join(" "))
-          left <> " " <> right
+        type = head |> Atom.to_string() |> String.capitalize()
+        args = rest |> Enum.map(&inspect(&1, true)) |> Enum.join(" ")
+
+        if in_type, do: ["(", type, " ", args, ")"], else: [type, " ", args]
+        |> Enum.join
       else
         inner = term |> Tuple.to_list() |> Enum.map(&inspect/1)
         "(" <> Enum.join(inner, ", ") <> ")"
       end
   end
 
-  def inspect(term) when is_list(term) do
+  def inspect(term, _) when is_list(term) do
     inner =
       term
       |> Enum.map(fn x -> inspect(x) end)
@@ -42,5 +44,6 @@ defmodule Elchemy.Format do
     "[" <> inner <> "]"
   end
 
-  def inspect(x), do: to_string(x)
+  def inspect(x, _) when is_atom(x), do: Atom.to_string(x) |> String.capitalize
+  def inspect(x, _), do: to_string(x)
 end
