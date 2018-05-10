a800013232daba6a0fd59a1a6f6ddf4f9fbd20c0
Krzysztof Wende <krzysztof.wende@gmail.com>
2018-05-10 22:17:31 +0200

    no plugin test

--- test/exunit_plugin_test.exs
+++ test/exunit_plugin_test.exs
@@ -2,12 +2,7 @@ defmodule ExUxunitPluginTest do
   use Elchemy
   use ExUnit.Case
 
-  alias Elchemy.Elixir.Plugin
-  alias Elchemy.Elixir.NativePlugin
   require Elchemy.Plugins.ExUnit, as: ExUnitPlugin
-
-  doctest Plugin
-  typetest Plugin
 
   ExUnitPlugin.test(Elchemy.Tests.ExUnitTest.Meta)
 
