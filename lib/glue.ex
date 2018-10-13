52d144e40c46a702cf7e1f8f3a04333f5515d0c7
Krzysztof Wende <iraasta@gmail.com>
2018-10-13 16:24:16 +0200

    Disable for Elixir 1.7

--- lib/glue.ex
+++ lib/glue.ex
@@ -3,7 +3,7 @@ defmodule Elchemy.Glue do
 
   def try_catch(func) do
     try do
-      {:ok, func.()}
+        {:ok, func.()}
     rescue
       e -> {:error, e.message}
     end
@@ -31,28 +31,29 @@ defmodule Elchemy.Glue do
 
   defp do_update_in([s | rest], f, target),
     do: Map.put(target, s, do_update_in(rest, f, target[s]))
+  defp do_update_in([], f, target),
+    do: f.(target)
 
-  defp do_update_in([], f, target), do: f.(target)
 
   defmacro curry({:/, _, [{name, _, _}, arity]}, opts \\ []) do
     lambdas = opts[:lambdas] || []
-    args = 1..arity |> Enum.map(&{:"arg#{&1}", [], Elixir})
+    args =
+      1..arity |> Enum.map(&({:"arg#{&1}", [], Elixir}))
 
     define_curried(:def, name, args, lambdas)
   end
 
   defmacro curryp({:/, _, [{name, _, _}, arity]}) do
-    args = 1..arity |> Enum.map(&{:"arg#{&1}", [], Elixir})
+    args =
+      1..arity |> Enum.map(&({:"arg#{&1}", [], Elixir}))
 
     define_curried(:defp, name, args)
   end
 
   defmacro defcurry(definition, _opts \\ [], do: body) do
     {fun, args} = Macro.decompose_call(definition)
-
     quote do
       unquote(define_curried(:def, fun, args))
-
       def unquote(fun)(unquote_splicing(args)) do
         unquote(body)
       end
@@ -61,10 +62,8 @@ defmodule Elchemy.Glue do
 
   defmacro defcurryp(definition, _opts \\ [], do: body) do
     {fun, args} = Macro.decompose_call(definition)
-
     quote do
       unquote(define_curried(:defp, fun, args))
-
       defp unquote(fun)(unquote_splicing(args)) do
         unquote(body)
       end
@@ -80,58 +79,36 @@ defmodule Elchemy.Glue do
 
     single_param_guard =
       function_args
-      |> Enum.map(fn {arg, arity} ->
-        quote do
-          is_function(unquote(arg), unquote(arity))
-        end
-      end)
-      |> Enum.reduce(true, fn l, acc ->
-        quote do
-          unquote(acc) and unquote(l)
-        end
-      end)
+      |> Enum.map(fn {arg, arity} -> quote do is_function(unquote(arg), unquote(arity)) end end)
+      |> Enum.reduce(true, fn l, acc -> quote do unquote(acc) and unquote(l) end end)
 
-    body =
+    body = quote do unquote(name)(unquote_splicing(args)) end
+
+    assigns = for {arg, arity} <- function_args do
+      application = quote do: unquote(arg).(unquote_splicing(take_arguments(arity)))
+      quote do unquote(arg) = unquote(do_curry(application, take_arguments(arity))) end
+    end
+
+    delambdified = if lambdas == [] do
+      nil
+    else
       quote do
-        unquote(name)(unquote_splicing(args))
-      end
-
-    assigns =
-      for {arg, arity} <- function_args do
-        application = quote do: unquote(arg).(unquote_splicing(take_arguments(arity)))
-
-        quote do
-          unquote(arg) = unquote(do_curry(application, take_arguments(arity)))
+        def unquote(name)(unquote_splicing(args)) when unquote(single_param_guard) do
+          unquote(assigns)
+          unquote(name)(unquote_splicing(args))
         end
       end
-
-    delambdified =
-      if lambdas == [] do
-        nil
-      else
-        quote do
-          def unquote(name)(unquote_splicing(args)) when unquote(single_param_guard) do
-            unquote(assigns)
-            unquote(name)(unquote_splicing(args))
-          end
-        end
-      end
+    end
 
     quote do
       def unquote(name)() do
         unquote(do_curry(body, args))
       end
-
       unquote(delambdified)
     end
   end
-
   defp define_curried(:defp, name, args) do
-    body =
-      quote do
-        unquote(name)(unquote_splicing(args))
-      end
-
+    body = quote do unquote(name)(unquote_splicing(args)) end
     quote do
       defp unquote(name)() do
         unquote(do_curry(body, args))
@@ -140,11 +117,8 @@ defmodule Elchemy.Glue do
   end
 
   defp do_curry(body, [h | args]) do
-    quote do
-      fn unquote(h) -> unquote(do_curry(body, args)) end
-    end
+    quote do fn unquote(h) -> unquote(do_curry(body, args)) end end
   end
-
   defp do_curry(body, []) do
     quote do: unquote(body)
   end
@@ -163,10 +137,10 @@ defmodule Elchemy.Glue do
   ```
   """
 
-  defmacro defreverse({name, _, _}, to: {:/, _, [{function, call_meta, _}, arity]}) do
+  defmacro defreverse({name, _,_}, to: {:/, _, [{function, call_meta, _ }, arity]} ) do
     args = take_arguments(arity)
-    function_definition = {name, call_meta, args}
-    call = {function, call_meta, Enum.reverse(args)}
+    function_definition = { name, call_meta, args}
+    call = {function, call_meta, Enum.reverse(args) }
 
     quote do
       def unquote(function_definition), do: unquote(call)
@@ -180,11 +154,11 @@ defmodule Elchemy.Glue do
   def foldl(x1, x2, x3), do: List.foldl(x3, x1, x2)
   """
 
-  defmacro defswap({name, _, _}, to: {:/, _, [{function, call_meta, _}, arity]}) do
+  defmacro defswap({name, _,_}, to: {:/, _, [{function, call_meta, _ }, arity]}) do
     args = take_arguments(arity)
-    function_definition = {name, call_meta, args}
-    {first, [second]} = Enum.split(args, arity - 1)
-    call = {function, call_meta, [second | first]}
+    function_definition = { name, call_meta, args}
+    {first, [second]} = Enum.split(args,arity-1)
+    call = {function, call_meta, [second | first] }
 
     quote do
       def unquote(function_definition), do: unquote(call)
@@ -193,36 +167,36 @@ defmodule Elchemy.Glue do
 
   ## TYPE CHECKING
   defmacro verify(as: {:/, _, [{call, _, []}, arity]}) do
-    {mod, function} =
-      case call do
-        {:., _, [{:__aliases__, _, mods}, fun]} ->
-          mod = Module.concat(mods)
-          {mod, fun}
+    {mod, function} = case call do
+      {:., _, [{:__aliases__, _, mods}, fun]} ->
+        mod = Module.concat(mods)
+        {mod, fun}
+      {:., _, [mod, fun]} ->
+        {mod, fun}
+    end
+    if System.version() |> String.starts_with?("1.7") do
+      :ok
+    else
+      quote do
+        spec_ast = Module.get_attribute(__MODULE__, :spec) |> hd |> elem(1)
 
-        {:., _, [mod, fun]} ->
-          {mod, fun}
-      end
+        {left, right} = case Kernel.Typespec.translate_spec(:spec, spec_ast, __ENV__) do
+          # Elixir <= 1.5.x
+          {{:spec, {fun1, _}, spec}, _line} ->
+            left = {{fun1, unquote(arity)}, [spec]}
+            right = {unquote(mod), unquote(function), unquote(arity)}
+            {left, right}
 
-    quote do
-      spec_ast = Module.get_attribute(__MODULE__, :spec)
-
-        {left, right} =
-          case Kernel.Typespec.translate_spec(:spec, spec_ast, __ENV__) do
-            # Elixir <= 1.5.x
-            {{:spec, {fun1, _}, spec}, _line} ->
-              left = {{fun1, unquote(arity)}, [spec]}
-              right = {unquote(mod), unquote(function), unquote(arity)}
-              {left, right}
-
-            # Elixir 1.6.x
-            {:spec, {fun1, _}, _l, spec} ->
-              right = {unquote(mod), unquote(function), unquote(arity)}
-              left = {{fun1, unquote(arity)}, [spec]}
-              {left, right}
-          end
+          # Elixir 1.6.x
+          {:spec, {fun1, _}, _l, spec} ->
+            right = {unquote(mod), unquote(function), unquote(arity)}
+            left = {{fun1, unquote(arity)}, [spec]}
+            {left, right}
+        end
 
         __MODULE__
         |> Module.put_attribute(:verify_type, [left, right, __MODULE__, unquote(mod)])
+      end
     end
   end
 
@@ -235,58 +209,46 @@ defmodule Elchemy.Glue do
 
   ## -- Mutually recursive Let..in using Y combinator
   defmacro let(clauses) do
-    right =
-      for {name, _} <- clauses do
-        quote do
-          &fun.(unquote(name)).(&1)
-        end
+    right = for {name, _} <- clauses do
+      quote do
+        &(fun.(unquote(name)).(&1))
       end
+    end
 
     names = for {n, _} <- clauses, do: Macro.var(n, nil)
     underscored = for _ <- names, do: Macro.var(:_, nil)
 
-    whole =
-      quote do
-        {unquote_splicing(names)} = {unquote_splicing(right)}
-        {unquote_splicing(underscored)} = {unquote_splicing(names)}
-      end
+    whole = quote do
+      {unquote_splicing(names)} = {unquote_splicing(right)}
+      {unquote_splicing(underscored)} = {unquote_splicing(names)}
+    end
 
-    inner =
-      {:fn, [],
-       for {name, c} <- clauses do
-         {:->, [],
-          [
-            [name],
-            quote do
-              unquote(whole)
-              unquote(c)
-            end
-          ]}
-       end}
+    inner = {:fn, [], for {name, c} <- clauses do
+      {:'->', [], [[name], quote do
+          unquote(whole)
+          unquote(c)
+      end]}
+    end}
 
     quote do
-      fun = rec(fun, unquote(inner))
+      fun = rec fun, unquote(inner)
       unquote(whole)
     end
   end
 
   ## -- Y-combinator
 
-  defmacro rec(name, {:fn, meta, [c | _clauses]} = f) do
+  defmacro rec(name, {:fn, meta, [c|_clauses]} = f) do
     var = if is_atom(name), do: Macro.var(name, nil), else: name
     n = num_args(c)
     args = n_args(n, [])
-
-    namedf =
-      quote do
-        fn unquote(var) ->
-          _ = unquote(var)
-          unquote(f)
-        end
+    namedf = quote do
+      fn (unquote(var)) ->
+        _ = unquote(var)
+        unquote(f)
       end
-
+    end
     combinator_fun = combinator_ast(namedf, args, meta)
-
     quote do
       combinator = unquote(combinator_fun)
       combinator.(combinator)
@@ -295,16 +257,7 @@ defmodule Elchemy.Glue do
 
   defp combinator_ast(namedf, args, meta) do
     x = Macro.var(:x, __MODULE__)
-
-    inner =
-      fn_ast(
-        args,
-        quote do
-          unquote(x).(unquote(x)).(unquote_splicing(args))
-        end,
-        meta
-      )
-
+    inner = fn_ast(args, quote do unquote(x).(unquote(x)).(unquote_splicing(args)) end, meta)
     quote do
       fn unquote(x) -> unquote(namedf).(unquote(inner)) end
     end
@@ -326,6 +279,7 @@ defmodule Elchemy.Glue do
   defp num_args_fn_head([{:when, _, args} | _]), do: Enum.take_while(args, &is_arg/1) |> length()
   defp num_args_fn_head(vars) when is_list(vars), do: length(vars)
 
-  defp is_arg({a, _m, c}) when is_atom(a) and is_atom(c), do: true
-  defp is_arg(_), do: false
+  defp is_arg({a, _m, c}) when is_atom(a) and is_atom(c), do: :true
+  defp is_arg(_), do: :false
+
 end
